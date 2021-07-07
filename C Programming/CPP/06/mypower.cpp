57400db4aeee3ac20f7ab5a9f48d19b25678158d
amamov <ysangsuk78@gmail.com>
2021-07-07 10:05:29 +0900

    [update] formatter

--- C Programming/CPP/06/mypower.cpp
+++ C Programming/CPP/06/mypower.cpp
@@ -2,18 +2,15 @@
 
 using namespace std;
 
-int power(int down, int up)
-{
+int power(int down, int up) {
     int result = down;
-    for (int i = 1; i < up; i++)
-    {
+    for (int i = 1; i < up; i++) {
         result *= down;
     }
     return result;
 }
 
-int main()
-{
+int main() {
     cout << power(2, 4) << endl;
     return 0;
 }
